e3d111b43180b540cf4768396d8d50de2704ac0a
prashantL1212 <lanke.prashant1111@gmail.com>
2025-09-09 00:49:50 +0530

    set up the dbContext class

---
+++ DbOperationEFCORE/DbOperationEFCORE/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationEFCORE.Data
+{
+    public class AppDbContext: DbContext
+    {
+
+    }
+}
--- DbOperationEFCORE/DbOperationEFCORE/Program.cs
+++ DbOperationEFCORE/DbOperationEFCORE/Program.cs
@@ -1,4 +1,7 @@
 
+using DbOperationEFCORE.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace DbOperationEFCORE
 {
     public class Program
@@ -7,6 +10,9 @@ namespace DbOperationEFCORE
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<AppDbContext>(options => 
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+            );
             // Add services to the container.
 
             builder.Services.AddControllers();
